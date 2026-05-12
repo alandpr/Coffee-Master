a6d458810cbb319fd6c4d3aa08667a3682134449
alandpr <alanpocos@gmail.com>
2026-05-12 12:10:47 -0300

    update

--- sw.js
+++ sw.js
@@ -1,31 +1,16 @@
-const CACHE_NAME = 'cm-v14-auto-stable';
-const assets = [
-  './',
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'cm-v16-final-manual';
+const assets = ['./', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // FORÇA A INSTALAÇÃO IMEDIATA
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('activate', event => {
-  // TOMA CONTROLE DE TODAS AS ABAS IMEDIATAMENTE
   event.waitUntil(clients.claim());
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
