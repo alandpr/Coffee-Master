62362af4828ab735bd8f5a94c004a14e9348946a
alandpr <alanpocos@gmail.com>
2026-05-12 11:41:55 -0300

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cm-v12-final-fix';
+const CACHE_NAME = 'cm-v13-auto-stable';
 const assets = [
   './',
   'index.html',
@@ -6,13 +6,15 @@ const assets = [
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  self.skipWaiting(); // FORÇA A INSTALAÇÃO IMEDIATA
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
 self.addEventListener('activate', event => {
+  // TOMA CONTROLE DE TODAS AS ABAS IMEDIATAMENTE
+  event.waitUntil(clients.claim());
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
